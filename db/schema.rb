8171c480718f49eb92b4402820114c693d2fb7a1
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-05 22:44:47 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180805223513) do
+ActiveRecord::Schema.define(version: 20180805224410) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180805223513) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "department"
   end
 
   create_table "patients", force: :cascade do |t|
