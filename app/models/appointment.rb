da051d9c09cf460a005fcae1b742d29da7590d37
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-06 00:01:10 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,6 +6,6 @@ class Appointment < ActiveRecord::Base
   def human_time
 
     # appointment_datetime.to_formatted_s(:long_ordinal)
-    appointment_datetime.strftime(“at %I:%M")
+    appointment_datetime.strftime("at %I:%M")
   end
 end
